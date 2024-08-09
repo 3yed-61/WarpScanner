c7651ce9a998982bfb899669be57d72a38f7aed7
3λΞĐ <git61@3yed.eu.org>
2024-08-09 06:11:27 +0330

    Update install-Linux.sh

--- install-Linux.sh
+++ install-Linux.sh
@@ -4,7 +4,7 @@ install_python() {
     echo "Installing Python..."
     sudo apt update -y || { echo "Failed to update packages. Exiting."; exit 1; }
     sudo apt install python3 -y || { echo "Failed to install Python. Exiting."; exit 1; }
-    sudo apt install python3-pip -y || { echo "Failed to install Python. Exiting."; exit 1; }
+    sudo apt install python3-pip -y || { echo "Failed to install pip. Exiting."; exit 1; }
 }
 
 install_git() {
@@ -22,48 +22,42 @@ install_curl() {
     sudo apt install curl -y || { echo "Failed to install curl. Exiting."; exit 1; }
 }
 
-if ! command -v python3 || ! command -v pip3; then
+# Check and install Python and pip
+if ! command -v python3 &>/dev/null || ! command -v pip3 &>/dev/null; then
     install_python
 fi
 
-if ! command -v git; then
+# Check and install Git
+if ! command -v git &>/dev/null; then
     install_git
 fi
 
-if ! command -v wget; then
+# Check and install wget
+if ! command -v wget &>/dev/null; then
     install_wget
 fi
 
-if ! command -v curl; then
+# Check and install curl
+if ! command -v curl &>/dev/null; then
     install_curl
 fi
 
+# Function to download the latest WarpScanner.py
+download_warp_scanner() {
+    echo "Downloading WarpScanner.py..."
+    curl -fsSL -o WarpScanner.py https://raw.githubusercontent.com/3yed-61/WarpScanner/main/WarpScanner.py || { echo "Failed to download WarpScanner.py. Exiting."; exit 1; }
+}
+
+# Check the existence and validity of WarpScanner.py
 if [ -f WarpScanner.py ]; then
     first_line=$(head -n 1 WarpScanner.py)
-    if [ "$first_line" == "import urllib.request" ]; then
+    if [[ "$first_line" == *"import urllib.request"* || "$first_line" != "V=31" ]]; then
         rm WarpScanner.py
-        echo "Updating WarpScanner.py..."
-        curl -fsSL -o WarpScanner.py https://raw.githubusercontent.com/3yed-61/WarpScanner/main/WarpScanner.py || { echo "Failed to download WarpScanner.py. Exiting."; exit 1; }
-        python3 WarpScanner.py
-        exit 0
+        download_warp_scanner
     fi
+else
+    download_warp_scanner
 fi
 
-if [ -f WarpScanner.py ]; then
-    first_line=$(head -n 1 WarpScanner.py)
-    if [ "$first_line" != "V=31" ]; then
-        rm WarpScanner.py
-        echo "Updating WarpScanner.py..."
-        curl -fsSL -o WarpScanner.py https://raw.githubusercontent.com/3yed-61/WarpScanner/main/WarpScanner.py || { echo "Failed to download WarpScanner.py. Exiting."; exit 1; }
-        
-        python3 WarpScanner.py
-        exit 0
-    else
-        python3 WarpScanner.py
-        exit 0
-    fi
-fi
-
-echo "Installing WarpScanner.py"
-curl -fsSL -o WarpScanner.py https://raw.githubusercontent.com/3yed-61/WarpScanner/main/WarpScanner.py || { echo "Failed to download WarpScanner.py. Exiting."; exit 1; }
+# Run WarpScanner.py
 python3 WarpScanner.py
